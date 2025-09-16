c8a8306500a8110e7d8ca7cc435b392d5a28aced
SAURABH271198 <saurabhtaragi30@gmail.com>
2025-09-16 14:18:53 +0530

    fix product card issue

--- src/app/home/featuredProducts.jsx
+++ src/app/home/featuredProducts.jsx
@@ -1,25 +1,26 @@
-import ProductCard from "../../components/products/productCard";
+import ProductCard from "../../components/products/ProductCard";
 
 const getFeaturedProducts = async () => {
-    try {
-        const resp = await fetch('https://fakestoreapi.com/products?limit=6');
-        return await resp.json()
-    }
-    catch(error) {
-        console.log(error);
-    }
-}
+	try {
+		const resp = await fetch("https://fakestoreapi.com/products?limit=6");
+		return await resp.json();
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 export default async function FeaturedProducts() {
-    const featuredProducts = await getFeaturedProducts();
-  return (
-    <section className="max-w-7xl mx-auto px-4 py-16">
-      <h2 className="text-3xl font-bold mb-8 text-gray-900">Featured Products</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {featuredProducts.map((product) => (
-          <ProductCard key={product.id} {...product} />
-        ))}
-      </div>
-    </section>
-  );
+	const featuredProducts = await getFeaturedProducts();
+	return (
+		<section className="max-w-7xl mx-auto px-4 py-16">
+			<h2 className="text-3xl font-bold mb-8 text-gray-900">
+				Featured Products
+			</h2>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+				{featuredProducts.map((product) => (
+					<ProductCard key={product.id} {...product} />
+				))}
+			</div>
+		</section>
+	);
 }
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,22 +1,21 @@
-import ProductCard from "../../components/products/productCard";
+import ProductCard from "../../components/products/ProductCard";
 
 const getProducts = async () => {
-    try {
-        const resp = await fetch('https://fakestoreapi.com/products?limit=12');
-        return await resp.json()
-    }
-    catch(error) {
-        console.log(error);
-    }
-}
+	try {
+		const resp = await fetch("https://fakestoreapi.com/products?limit=12");
+		return await resp.json();
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 export default async function ProductList() {
-    const products = await getProducts();
-    return(
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-            {products?.map((product) => (
-                <ProductCard key={product.id} {...product} />
-            ))}
-        </div>
-    )
+	const products = await getProducts();
+	return (
+		<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+			{products?.map((product) => (
+				<ProductCard key={product.id} {...product} />
+			))}
+		</div>
+	);
 }
